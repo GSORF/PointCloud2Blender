d6606bdc66d3cd11e907b4181ed914ea35fe020e
GSORF <GSORF@LwTV.de>
2015-02-15 21:50:33 +0100

    Added FileOpenDialog for choosing the point cloud file

--- PointCloud2Blender/mainwindow.cpp
+++ PointCloud2Blender/mainwindow.cpp
@@ -6,9 +6,48 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->btnFileOpenDialog, SIGNAL(clicked()), this, SLOT(showFileOpenDialog()));
+    connect(ui->btnImport, SIGNAL(clicked()), this, SLOT(startFileImport()));
+
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::showFileOpenDialog()
+{
+    //The following filetypes are selectable
+    QString fileFormat = "XYZ Files (*xyz);;XYZ Binary Files (*xyzb);;All Files (*.*)";
+    QString fileName = "";
+
+    fileName = QFileDialog::getOpenFileName(this, "Please specify your point cloud file", QDir::homePath(), fileFormat);
+
+    if(!fileName.isEmpty())
+    {
+        ui->txtFilePathImport->setText(fileName);
+        ui->btnImport->setEnabled(true);
+    }
+    else
+    {
+        ui->btnImport->setEnabled(false);
+    }
+}
+
+void MainWindow::startFileImport()
+{
+    qDebug() << "MainWindow::startFileImport()";
+
+    //create a new Thread for importing
+    //pass the filename to the importer Thread (Name: ImportWorker)
+    //start the thread
+
+    //Later: Connect the importer Thread with the main data container that holds panorama information (or more general: the 3D Point Cloud), (Name: Panorama3D)
+
+    //Everything else happens on the Thread (Importing, maybe calculating spherical coordinates, and sending over the data to the main data container
+
 }
--- PointCloud2Blender/mainwindow.h
+++ PointCloud2Blender/mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 
+#include <QDebug>
+
+#include <QFileDialog>
+
 namespace Ui {
 class MainWindow;
 }
@@ -11,12 +15,21 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+    //Create a new ImportWorkerThread
+    //Create a ThreadPool
+    //...
+
+
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+
+public slots:
+    void showFileOpenDialog();
+    void startFileImport();
 };
 
 #endif // MAINWINDOW_H
